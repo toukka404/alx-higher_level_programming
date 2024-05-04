d626fbfe5430e1830a735c2c916dbcb15e0cc819
toukka404 <toukkakhaled98@gmail.com>
2024-05-04 08:36:22 -0700

    push a

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,8 @@
 #!/usr/bin/node
-'use strict';
-if (!process.argv[3]) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  const a = process.argv; let highest = parseInt(a[2]); let second = parseInt(a[3]);
-  if (second > highest) {
-    highest = second;
-    second = parseInt(a[2]);
-  }
-  let i = 3;
-  while (parseInt(a[i])) {
-    if (parseInt(a[i]) > highest) {
-      highest = parseInt(a[i]);
-    }
-    i++;
-  }
-  i = 3;
-  while (parseInt(a[i])) {
-    if (second < parseInt(a[i]) && parseInt(a[i]) < highest) {
-      second = parseInt(a[i]);
-    }
-    i++;
-  }
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
